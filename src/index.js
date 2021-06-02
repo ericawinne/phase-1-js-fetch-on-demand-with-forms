9c07cc65585b4c06f12f7bdbc3bb4dff1bd2728f
Ivan Candido <ivan.candido@powerhrg.com>
2021-06-02 10:04:36 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ const init = () => {
         const input = document.querySelector('input#searchByID');
       
         console.log(input.value);
-      });
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+          const title = document.querySelector('section#movieDetails h4');
+          const summary = document.querySelector('section#movieDetails p');
+
+          title.innerText = data.title
+          summary.innerText = data.summary
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
