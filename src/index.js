aa5772b97f5f70b10daa19a0a7e124d7cdd27253
Erica Winne <erica.winne@powerhrg.com>
2021-06-02 09:40:33 -0400

    fetch on demand

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+      
+        console.log(input.value);
+      });
 }
 
 document.addEventListener('DOMContentLoaded', init);
